ed481f7d2bac4a6d66ca91aa30014dd08f95693f
Khang Duy <144929763+khangbkk23@users.noreply.github.com>
2025-05-10 14:26:35 +0700

    Update sys_killall.c

--- ossim_sierra - Copy/ossim_sierra/src/sys_killall.c
+++ ossim_sierra - Copy/ossim_sierra/src/sys_killall.c
@@ -17,88 +17,102 @@
 #include <queue.h>
 #include <sched.h>
 
-int __sys_killall(struct pcb_t *caller, struct sc_regs* regs) {
+int __sys_killall(struct pcb_t *caller, struct sc_regs *regs) {
     char proc_name[100];
-    uint32_t data;
+    uint32_t data, memrg = regs->a1;
 
-    //hardcode for demo only
-    uint32_t memrg = regs->a1; // ID of mem region that store proc naame need to kill
-    
     /* TODO: Get name of the target proc */
-    //proc_name = libread..
     int i = 0;
     data = 0;
-    while (data != -1) {
+    while (1) {
         libread(caller, memrg, i, &data);
-        proc_name[i]= data;
-        printf("Syskill all iteration %d, read data = %d, proc_name[%d] = %d\n",i,data,i,data);
-        if(data == -1) proc_name[i]='\0';
-        i++;
+        if (data == (uint32_t)-1 || i >= sizeof(proc_name) - 1) {
+            proc_name[i] = '\0';
+            break;
+        }
+        proc_name[i++] = (char)data;
     }
-    printf ("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
+
+    printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
 
     /* TODO: Traverse proclist to terminate the proc
      *       stcmp to check the process match proc_name
      */
-    struct pcb_t *k_process [MAX_PRIO]; // this list to add processed required to kill
+    struct pcb_t *k_process[MAX_PROC];
     int index = 0;
- 
+
     struct queue_t *run_list = caller->running_list;
     struct queue_t *mlq = caller->mlq_ready_queue;
-    printf("run_list size: %d\n",run_list->size);
 
-    for (int i =0; i<run_list->size; ++i){
-        char *proc_get_name = strrchr(run_list->proc[i]->path, '/');  //skip 2 /
-        if (proc_get_name && strcmp(proc_get_name+1,proc_name) == 0){   //move the pointer from / to the first char of name
+    printf("run_list size: %d\n", run_list->size);
+
+    // Traverse run_list
+    for (int i = 0; i < run_list->size; ++i) {
+        char *proc_get_name = strrchr(run_list->proc[i]->path, '/');
+        if (proc_get_name)
+            proc_get_name++;
+        else
+            proc_get_name = run_list->proc[i]->path;
+
+        if (strcmp(proc_get_name, proc_name) == 0) {
             k_process[index++] = run_list->proc[i];
-            printf("Found process name %s to kill in run list\n",proc_get_name);
-            run_list->proc[i]->pc = run_list->proc[i]->code->size; // set program counter = size to force the process to end
-            for(int j=i; j < run_list->size - 1;j++){
-                run_list->proc[j] = run_list->proc[j+1];  // Remove process form queue by shifting array
-             }
+            printf("Found process name %s to kill in run list\n", proc_get_name);
+
+            run_list->proc[i]->pc = run_list->proc[i]->code->size;
+
+            for (int j = i; j < run_list->size - 1; j++)
+            {
+                run_list->proc[j] = run_list->proc[j + 1];
+            }
+
             run_list->size--;
-            i--; //to check the newly shifted process, avoid skipping it
+            i--;
         }
     }
 
-    for (int i = 0; i < MAX_PRIO; ++i) {
-        for (int j = 0; j < mlq[i].size; ++j) {
-            char *mlq_proc_get_name = strrchr(mlq[i].proc[j]->path, '/');
-            if (mlq_proc_get_name && strcmp(mlq_proc_get_name + 1, proc_name) == 0) {
-                //  pthread_mutex_lock(&queue_lock);
-                printf("mlq[%d] size = %d\n", i, mlq[i].size);
-                k_process[index++] = mlq[i].proc[j];
+    // Traverse mlq
+    for (int prio = 0; prio < MAX_PRIO; ++prio) {
+        for (int j = 0; j < mlq[prio].size; ++j) {
+            char *mlq_proc_get_name = strrchr(mlq[prio].proc[j]->path, '/');
+            if (mlq_proc_get_name)
+                mlq_proc_get_name++;
+            else
+                mlq_proc_get_name = mlq[prio].proc[j]->path;
+
+            if (strcmp(mlq_proc_get_name, proc_name) == 0) {
+                printf("mlq[%d] size = %d\n", prio, mlq[prio].size);
+                k_process[index++] = mlq[prio].proc[j];
                 printf("Found process name %s to kill in mlq\n", mlq_proc_get_name);
-                mlq[i].proc[j]->pc = mlq[i].proc[j]->code->size; // set program counter = size to force the process to end
-                for (int k = 0; k < mlq[i].size - 1; k++)
-                {
-                    mlq[i].proc[k] = mlq[i].proc[k + 1]; // Remove process form queue by shifting array
+
+                mlq[prio].proc[j]->pc = mlq[prio].proc[j]->code->size;
+
+                for (int k = j; k < mlq[prio].size - 1; k++) {
+                    mlq[prio].proc[k] = mlq[prio].proc[k + 1];
                 }
-                mlq[i].size--;
-                j--; // to check the newly shifted process, avoid skipping it
-                // pthread_mutex_unlock(&queue_lock);
+
+                mlq[prio].size--;
+                j--;
             }
         }
     }
-    //caller->running_list
-    //caller->mlq_ready_queu
 
-    /* TODO Maching and terminating 
+    /* TODO Maching and terminating
      *       all processes with given
      *        name in var proc_name
      */
-    if (index > 0){
-        printf("Remove %d process from mlq and run queue,ready to free\n",index);
-        for(int i=0;i<index;i++) {
-            printf("Free ALlocated region for process %s with ID=%d in kill list\n",proc_name,i);
-            for(int j=0;j < k_process[i]->code->size;j++)
-            if (k_process[i]->code->text[j].opcode==ALLOC) {  //Free all allocated region of killed process
-               __free(k_process[i],k_process[i]->mm->mmap->vm_id, k_process[i]->code->text[j].arg_0);
+    if (index > 0) {
+        printf("Remove %d process from mlq and run queue, ready to free\n", index);
+        for (int i = 0; i < index; i++) {
+            printf("Free ALlocated region for process %s with ID=%d in kill list\n", proc_name, i);
+            for (int j = 0; j < k_process[i]->code->size; j++) {
+                if (k_process[i]->code->text[j].opcode == ALLOC) {
+                    __free(k_process[i], k_process[i]->mm->mmap->vm_id, k_process[i]->code->text[j].arg_0);
+                }
             }
         }
     }
-    else
-        printf("Process with name %s is not exist\n",proc_name);
-
-    return 0; 
+    else {
+        printf("Process with name %s does not exist\n", proc_name);
+    }
+    return 0;
 }
